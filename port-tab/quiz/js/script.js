8a4c21a6aa11b1624bd638e2bf2ed8bcb55ddaf8
aakinpelu <aoakinpelu@gmail.com>
2015-05-18 02:38:41 -0400

    quiz update

--- port-tab/quiz/js/script.js
+++ port-tab/quiz/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var key = {
-		answers: ["Hayes", "D.C.", "US and Japan", "Apartheid", "1960"]
+		answers: ["Hayes", "D.C.", "US and Japan", "Apartheid", "England", "1960"]
 	};
 
 	var progress = $('#progress'),
@@ -12,28 +12,29 @@ $(document).ready(function() {
 		questionLength = answers.length,
 		questions = $('.question');
 
-	function roundScore(num, dec) {
-		var score = Math.round(num * Math.pow(10,dec)) / Math.pow(10,dec);
+	function roundScore(num, digits) {
+		var score = Math.round(num * Math.pow(10, digits)) / Math.pow(10, digits);
 		return score;
 	}
 
 	function scoreComment(score) {
 		var comment;
 
-		if(score == 100) {
+		if (score > 80) {
 			comment = "Well done!";
-		} else if (score < 80 && score > 60) {
+		} else if (score <= 80 && score >= 60) {
 			comment = "Not bad...";
 		} else {
 			comment = "You did terrible.";
 		}
+		return comment;
 	}
 
 	function checkAnswer() {
 		var resultsArray = [];
 		var correct = false;
 
-		for (i = 0; i < answers.length; i++) {
+		for (var i = 0; i < answers.length; i++) {
 			if (answers[i] == inputAnswers[i]) {
 				correct = true;
 			} else {
@@ -46,16 +47,16 @@ $(document).ready(function() {
 
 	$('.start').click(function() {
 	    $(this).parents('.question-section').fadeOut(500, function() {
-	        $(this).next().fadeIn(500, function() {
-	        	progress.fadeIn(500);
+	        $(this).next().fadeIn(100, function() {
+	        	progress.fadeIn(1000);
 	        });
 	    });
 	    return false;
 	});
 
-	$('.choices li input').click(function() { 
-		$(this).parents('.choices').children('li').removeClass("selected"); 
-		$(this).parents('li').addClass('selected'); 
+	$('.choices .radio input').click(function() { 
+		$(this).parents('.choices').children('.radio').removeClass("selected"); 
+		$(this).parents('.radio').addClass('selected'); 
 	});
 
 	$('.next').click(function() {
@@ -72,8 +73,7 @@ $(document).ready(function() {
 	        $(this).next().fadeIn(500);
 	    });
 
-	    progressBar.animate({width: progressBar.width() + Math.round(barWidth / questionLength), }, 500);
-	    
+	    progressBar.animate({width: progressBar.width() + Math.round(barWidth / questionLength), }, 500);  
 	    return false;
 	});
 
@@ -85,11 +85,12 @@ $(document).ready(function() {
 	    });
 
 	    progressBar.animate({width: progressBar.width() - Math.round(barWidth / questionLength), }, 500);
-	    
 	    return false;
 	});
 
 	$('.finish').click(function() {
+		progressBar.animate({width: progressBar.width() + Math.round(barWidth / questionLength), }, 500);
+
 		var checked = $(this).parents('.question-section').find('input[type=radio]:checked');
 
 		if (checked.length === 0) {
@@ -97,31 +98,33 @@ $(document).ready(function() {
 			return false;
 		}
 
+		notice.remove();
+
 		var checkedArray = $('input[type=radio]:checked');
 
-		for (var i = 0; i <= checkedArray.length; i++) {
+		for (var i = 0; i < checkedArray.length; i++) {
 			inputAnswers.push(checkedArray[i].getAttribute('data-key'));
 		}
 
 		var results = checkAnswer(),
 			resultString = "",
 			count = 0,
-			answerKey = "Answers: <br/>",
+			answerKey = "<h3 class='answersHeader'>Answers:</h3>",
 			score;
 
-		for (var j = 0; j <= results.length; j++) {
+		for (var j = 0; j <= results.length - 1; j++) {
 			if (results[j] === true) {
 				count++;
 			}
 
 			resultString += '<div class="resultRow"> Question #' + (j + 1) + (results[j] === true ? "<div class='correct'><span>Correct</span></div>": "<div class='incorrect'><span>Incorrect</span></div>") + "</div>"; 
-    		answerKey += (j + 1) + " : " + answers[j] + ' &nbsp; &nbsp; &nbsp;';
+    		answerKey += "#" + (j + 1) + " - " + answers[j] + '</br>';
 		}
 
-		score = roundScore(count / (questionLength * 100), 2);
+		score = roundScore(count / questionLength * 100, 2);
 
 		answerKey = "<div id='answer-key'>" + answerKey + "</div>"; 
-		results = '<h2 class="qTitle">' + scoreComment(score) + ' You scored ' + score + '%</h2>' + resultString + answerKey; 
+		resultString = '<h2 class="resultsHeader">' + scoreComment(score) + ' You scored ' + score + '%</h2>' + resultString + answerKey; 
 		$('#results').html(resultString).show(); 
 
 		$(this).parents('.question-section').fadeOut(500, function(){ 
